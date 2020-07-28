58dd7989da7545b5d36ee1660a145b21f4cb57a6
xRokz <38587835+xrokz@users.noreply.github.com>
2020-07-28 19:29:51 +0300

    skeleton done
    Now you can send messages in one room only...

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,23 @@ const socket = io('/');
 console.log(name);
 socket.emit("user-join", name);
 
+function send(e) {
+    // console.log($("#msg")[0].which);
+    if(e.which == 13) {
+        //submit form via ajax, this is not JS but server side scripting so not showing here
+        socket.emit("send-message", {name, msg: $("#msg").val()})
+        $("#msg").val("");
+        e.preventDefault();
+    }
+}
+$(function() {
+    $("#msg").keypress((e) => {
+        send(e);
+    })
+})
+socket.on("new-message", data => {
+    $("#area").html($("#area").html() + `<br/>${data.name}: ${data.msg}`)
+})
 socket.on("user-connected", username => {
     console.log("New User: ", username);
 })
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const socket = require("socket.io")(80);
 const app = express();
+const server = require("http").Server(app);
+const socket = require("socket.io")(server);
+
 app.set("view engine", "ejs");
 const port = 3000;
 
 app.use("/public", express.static("./public"));
-app.use("/", express.static("./node_modules"));
+// app.use("/", express.static("./node_modules"));
 app.get('/', (req, res) => res.render('index', {}));
 
 app.get("/room", (req, res) => {
@@ -15,9 +17,13 @@ app.get("/room", (req, res) => {
 });
 
 socket.on("connection", io => {
-    io.on("user-send", user => {
-        console.log(user);
+    io.on("user-join", user => {
+        socket.emit("user-connected", user)
+    });
+    
+    io.on("send-message", data => {
+        socket.emit("new-message", data);
     });
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+server.listen(port, () => console.log(`WebChat app listening at http://localhost:${port} !`));
